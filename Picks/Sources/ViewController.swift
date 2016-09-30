3355266a861b943663fab91f706d7d6cb8425c73
Jinhong Kim <jh.k@navercorp.com>
2016-09-30 15:52:26 +0900

    Project Setup

---
+++ Picks/Sources/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Picks
+//
+//  Created by Jinhong Kim on 9/30/16.
+//  Copyright © 2016 JHK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
